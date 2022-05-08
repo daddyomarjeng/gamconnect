65e7a346dd11e68ef5edb02865b2be7298cc2381
DOJ <omarjeng999@gmail.com>
2022-05-08 16:17:24 +0000

    finish single post UI

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,66 @@
-import { Box } from "@mui/material";
+import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import DOJImg from "../assets/0.jpg";
+import PostImg from "../assets/p.jpg";
 
 const Feed = () => {
   return (
-    <Box bgcolor="blue" flex={4} p={2}>
-      Feed
+    <Box flex={4} p={2}>
+      <Card>
+        {/* top of card */}
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="feed-avatar">
+              DOJ
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+        />
+        {/* image */}
+        <CardMedia
+          component="img"
+          height="20%"
+          image={PostImg}
+          alt="Post by DOJ"
+        />
+        {/* card text */}
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt a
+            quam itaque maxime tenetur quaerat hic assumenda labore animi, vel
+            iste unde ullam et qui voluptatem iure recusandae corporis non!
+          </Typography>
+        </CardContent>
+        {/* card action */}
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favourites">
+            {/* <Favorite /> */}
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: "red" }} />}
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+        </CardActions>
+      </Card>
     </Box>
   );
 };
